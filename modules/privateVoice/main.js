5e56d9f56b54d3779d11559f8e424d1293082a28
Tymen Vis (student) <s1171204@student.windesheim.nl>
2022-07-16 14:15:19 +0200

    Added some basic check

--- modules/privateVoice/main.js
+++ modules/privateVoice/main.js
@@ -1,3 +1,8 @@
+const getMembers = (channelState) => {
+    let memberCount = channelState.channel.members;
+    return memberCount
+}
+
 const privateVoice = async(oldState, newState, server, privateChannelID) => {
     const getVCNew = newState.member.voice.channel;
     const serverID = server.id
@@ -14,13 +19,17 @@ const privateVoice = async(oldState, newState, server, privateChannelID) => {
                 newState.member.voice.setChannel(channel.id)
             })
         }
+        if(getVCNew.parent.id  === category.id) {
+            const memberCount = getMembers(newState)
+            console.log(memberCount.size)
+            // if (memberCount == 2) {
+
+            // }
+        }
     }
     if (oldState.channel) {
         if(oldState.channel.parent.id  === category.id) {
-            let memberCount = 0;
-            oldState.channel.members.each(member=>{
-                memberCount++
-            })
+            const memberCount = getMembers(oldState).size
             if (memberCount == 0 ) {
                 oldState.channel.delete();
             }
