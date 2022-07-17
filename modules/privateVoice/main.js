8b989d57e39c0fc5cfe77feccd36393b102ccb25
Tymen Vis <Tymenvis@outlook.com>
2022-07-17 23:17:09 +0200

    small name change

--- modules/privateVoice/main.js
+++ modules/privateVoice/main.js
@@ -28,7 +28,7 @@ const privateVoice = async(onLeftState, onJoinState, server, privateChannelID) =
         let getParentId = await onJoinState.member.voice.channel.parentId;
 
         if (getVCJoin.id == privateChannelID) {
-            const voiceChannelName = `RESERVED VC`
+            const voiceChannelName = `reserved vc`
             await createPrivateVoice(server, onJoinState, getCategory, voiceChannelName)
         }
 
