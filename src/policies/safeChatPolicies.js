85bb59180dfc37348ebdb9e081c634fe9f41a3de
Tymen Vis <Tymenvis@outlook.com>
2022-09-06 19:01:43 +0200

    Fixed Safechat

--- src/policies/safeChatPolicies.js
+++ src/policies/safeChatPolicies.js
@@ -11,8 +11,7 @@ let sendError = (message) => {
 const safeChatPolicies = {
     isUserAllowedSafeChat: (server, message) => {
         return new Promise(async (resolve, reject) => {
-            await gPolicies.isServerMember(server, message).catch(err => { sendError(message); reject(err)}) && 
-            await gPolicies.hasConversationRole(message, server).catch(err => {sendError(message); reject(err)}) ? 
+            await gPolicies.isServerMember(server, message).catch(err => { sendError(message); reject(err)}) ? 
                 resolve(true) : reject(false)
         })
     }
